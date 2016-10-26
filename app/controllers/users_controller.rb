f8d3c94a44cdbb384440c941e7dc3982135ea2d4
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2016-10-26 17:19:21 -0400

    added variables to products and users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,31 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(param[:id])
+    if @user.save
+      redirect_to root
+    else
+      render :edit
+    end
   end
 
   def delete
+    @user = User.find(params[:id])
+    @user.destroy
   end
 end
