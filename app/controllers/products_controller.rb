9bc457146c9cbfb1270f85891a448328a6187b84
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2016-10-27 17:10:48 -0400

    added form for cre creating new items; added temporary display to product index page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,7 +14,7 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to root
+      redirect_to products_url
     else
       render :new
     end
