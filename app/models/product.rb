234630c8de2d8e8332c49d02c61e18d661abc384
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2016-10-30 02:35:58 -0400

    added validations to models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews
+
+  validates :name,:description,:price_in_cents, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   has_many :products, through: :reviews
 
   has_secure_password
+
+  validates :email, uniqueness: true
+  validates :email,:password,:password_confirmation presence: true
 end
