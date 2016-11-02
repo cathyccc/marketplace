803db80cbe70c09b73d7320a0a9cbe160e9bc830
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2016-11-02 11:29:54 -0400

    added delete button for each review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,7 +10,7 @@ class ReviewsController < ApplicationController
     @review.user = current_user
 
     if @review.save
-      redirect_to product_path(params[:id]), notice: 'Review added.'
+      redirect_to product_path(params[:product_id]), notice: 'Review added.'
     else
       render :product
     end
