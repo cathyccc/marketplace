ee0efde9bf5245b625f21ed0d1660e3e2fbb3e01
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2016-11-02 23:12:41 -0400

    added method to convert cents to dollars

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,8 @@ class Product < ApplicationRecord
   has_many :users, through: :reviews
 
   validates :name,:description,:price_in_cents, presence: true
+
+  def convert_dollars
+    price_in_cents / 100.00
+  end
 end
