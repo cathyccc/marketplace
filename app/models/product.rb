c22ba08074ec99c616c6ac03cfc2bce945157286
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2016-12-07 16:04:43 -0500

    added validation for price and have errors show on creating product

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
   has_many :users, through: :reviews
 
   validates :name,:description,:price_in_cents, presence: true
+  validates :price_in_cents, numericality: true, numericality: {greater_than: 0}
 
   mount_uploader :product_image, ProductImageUploader
 
