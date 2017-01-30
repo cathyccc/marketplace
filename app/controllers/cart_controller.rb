a69a425a8655e4cd72b29574171b0098cf590800
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2017-01-30 13:00:34 -0500

    add subtotal to cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -4,6 +4,7 @@ class CartController < ApplicationController
   def show
     @cart = Cart.find_by(cart_owner_id: @current_user.id, status_id: 1)
     @cart_products = @cart.cart_products.all.to_a
+    @subtotal = subtotal
   end
 
   def destroy
@@ -12,5 +13,9 @@ class CartController < ApplicationController
   private
   def cart_params
     params.require(:cart).permit(:user_id,:status_id)
+  end
+
+  def subtotal
+    @cart_products.inject(0) {|sum,item| sum+ Product.find(item.product_id).price_in_cents}
   end
 end
