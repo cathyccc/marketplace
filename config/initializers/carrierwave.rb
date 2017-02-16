9e89fa602ebb11446fd748f79fc0fc834e752fb7
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2017-02-16 17:51:50 -0500

    trying again with aws-fog gem

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,21 @@
-CarrierWave.configure do |config|                      # required
-  config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:      ENV["AWS_ACCESS_KEY_ID"],                        # required
-    aws_secret_access_key:  ENV["AWS_SECRET_ACCESS_KEY"],                        # required
-    # region:                'eu-west-1',                  # optional, defaults to 'us-east-1'
-    # host:                  's3.example.com',             # optional, defaults to nil
-    # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
-  }
-  config.fog_directory  = ENV["S3_BUCKET"]
+if Rails.env.test? or Rails.env.development?
+  CarrierWave.configure do |config|
+    config.storage = :file
+    config.root = "#{Rails.root}/tmp"
+    config.cache_dir = "#{Rails.root}/tmp/images"
+  end
+else
+  CarrierWave.configure do |config|                      # required
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider:              'AWS',                        # required
+      aws_access_key_id:      ENV["AWS_ACCESS_KEY_ID"],                        # required
+      aws_secret_access_key:  ENV["AWS_SECRET_ACCESS_KEY"],                        # required
+      # region:                'eu-west-1',                  # optional, defaults to 'us-east-1'
+      # host:                  's3.example.com',             # optional, defaults to nil
+      # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+    }
+    config.storage = :fog
+    config.fog_directory  = ENV["S3_BUCKET"]
+  end
 end
