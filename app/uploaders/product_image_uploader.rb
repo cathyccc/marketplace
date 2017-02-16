07d13b4d7ed4c5d7c97d9e67841eb95d5a5a6058
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2017-02-16 18:27:55 -0500

    reset s3 setup

--- app/uploaders/product_image_uploader.rb
+++ app/uploaders/product_image_uploader.rb
@@ -10,9 +10,13 @@ class ProductImageUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  # def store_dir
-  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  # end
+  def store_dir
+    "public/uploads"
+  end
+
+  def cache_dir
+    '/tmp/uploads'
+  end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
