6be5fffcc98ca06085009659cf9352c6c4aa7116
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2017-02-16 19:20:24 -0500

    moved fog creditentials up

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,15 +8,15 @@ CarrierWave.configure do |config|
 
   # Use AWS storage if in production
   if Rails.env.production?
-    CarrierWave.configure do |config|
-      config.storage = :fog
-    end
     config.fog_credentials = {
       :provider               => 'AWS',                             # required
       :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],          # required
       :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],      # required
       :region                 => 'ca-central-1'                     # optional, defaults to 'us-east-1'
     }
+    CarrierWave.configure do |config|
+      config.storage = :fog
+    end
     config.fog_directory  = ENV["S3_BUCKET"]                        # required
     #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
     #config.fog_public     = false                                  # optional, defaults to true
