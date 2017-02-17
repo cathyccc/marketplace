95ff30357bf850525599fed3d2dfdd361336124f
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2017-02-16 19:13:10 -0500

    changed region of s3 bucket to ca-central-1

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,25 +1,26 @@
-# Use local storage if in development or test
-if Rails.env.development? || Rails.env.test?
-  CarrierWave.configure do |config|
-    config.storage = :file
+CarrierWave.configure do |config|
+  # Use local storage if in development or test
+  if Rails.env.development? || Rails.env.test?
+    CarrierWave.configure do |config|
+      config.storage = :file
+    end
   end
-end
 
-# Use AWS storage if in production
-if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.storage = :fog
+  # Use AWS storage if in production
+  if Rails.env.production?
+    CarrierWave.configure do |config|
+      config.storage = :fog
+    end
+    config.fog_credentials = {
+      :provider               => 'AWS',                             # required
+      :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],          # required
+      :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],      # required
+      :region                 => 'ca-central-1'                     # optional, defaults to 'us-east-1'
+    }
+    config.fog_directory  = ENV["S3_BUCKET"]                        # required
+    #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
+    #config.fog_public     = false                                  # optional, defaults to true
+    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
   end
-end
 
-config.fog_credentials = {
-  :provider               => 'AWS',                             # required
-  :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],          # required
-  :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],      # required
-  :region                 => 'Montreal'                        # optional, defaults to 'us-east-1'
-}
-config.fog_directory  = ENV["S3_BUCKET"]                        # required
-#config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
-#config.fog_public     = false                                  # optional, defaults to true
-config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
